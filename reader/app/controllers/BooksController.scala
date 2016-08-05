f6c2be0dde37f8e5d6902255958e833aec857898
Krzysztof Pado <povder@gmail.com>
2016-08-05 19:24:18 +0200

    Fix controller for a reader monad

--- reader/app/controllers/BooksController.scala
+++ reader/app/controllers/BooksController.scala
@@ -4,8 +4,10 @@ import javax.inject.Inject
 
 import model.Book
 import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Result}
 import services.BooksService
+
+import scalaz._
 
 class BooksController @Inject()(booksService: BooksService) extends Controller {
 
@@ -23,12 +25,10 @@ class BooksController @Inject()(booksService: BooksService) extends Controller {
   }
 
   def updateTitle(id: Int) = Action(parse.text) { request =>
-    Book.get(id).map {
-      case Some(book) =>
-        book.copy(title = request.body).save()
-        NoContent
+    Book.get(id).flatMap {
+      case Some(book) => book.copy(title = request.body).save().map(_ => NoContent)
 
-      case None => NotFound
+      case None => Reader[BooksService, Result](_ => NotFound)
     }.run(booksService)
   }
 }
